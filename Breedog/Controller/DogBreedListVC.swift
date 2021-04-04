d42b4962fc5cdb65a2d1729d51d30ee74d18069b
Harpreet Singh <singharpreet92@yahoo.in>
2021-04-04 11:57:17 +1000

    code refactor

--- Breedog/Controller/DogBreedListVC.swift
+++ Breedog/Controller/DogBreedListVC.swift
@@ -18,21 +18,27 @@ class DogBreedListVC: AbstractCollectionListController {
     //MARK: - Server Data
     /// Get data from server and call when view did load  and show in collection cell
     override func requestItems(_ query: String, page: Int, completion: @escaping (Array<Any>?, NSError?, Bool?) -> Void) {
-        viewModel.fetchAllBreeds { (images, err) in
-            if err == nil {
+        viewModel.fetchAllBreeds {[weak self] (images, err) in
+            if images.count > 0 && err == nil {
                 DispatchQueue.main.async {
-                    self.items = images
-                    completion(self.items,nil,false)
+                    self?.items = images
+                    completion(self?.items,nil,false)
                 }
+            } else {
+                self?.noItemsText = "No data found."
+                completion([],nil,false)
             }
         }
-        completion([],nil,false)
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        viewModel.requestRandomImage
+        if let item = items[indexPath.item] as? BreedImage {
+            viewModel.fetchRandomImageForBreed(breed: item.breed) { (breed, error) in
+                DispatchQueue.main.async {
+                    self.items[indexPath.item] = breed
+                    self.collectionView.reloadItems(at: [indexPath])
+                }
+            }
+        }
     }
-
-    
-    
 }
--- Breedog/ViewModel/BreedFetchViewModel.swift
+++ Breedog/ViewModel/BreedFetchViewModel.swift
@@ -18,54 +18,47 @@ class BreedFetchViewModel {
 //        self.endPoint = endPoint
 //    }
     
-    init() {
-        APIManager.requestBreedsList { (arr, err) in
-            self.breeds = arr
-            print("got array")
-        }
-    }
-    
-    func fetchAllBreeds(completionHandler: @escaping ([BreedImage], Error?) -> Void) {
-        sleep(2)
-        let dispatchGroup = DispatchGroup()
+//    init() {
 //        APIManager.requestBreedsList { (arr, err) in
 //            self.breeds = arr
-            self.breeds.forEach { (breed) in
-                print("started")
-                dispatchGroup.enter()
-                APIManager.requestRandomImage(breed: breed) { (image, err) in
-                    print("image")
-                    let img = BreedImage(image: image!.message, breed: breed)
-                    self.allBreedImage.append(img)
-                    dispatchGroup.leave()
+//            print("got array")
+//        }
+//    }
+    
+    func fetchAllBreeds(completionHandler: @escaping ([BreedImage], Error?) -> Void) {
+        
+        let dispatchGroup = DispatchGroup()
+        APIManager.requestBreedsList { (breeds, error) in
+            if error == nil {
+                self.breeds = breeds
+                self.breeds.forEach { (breed) in
+                    print("started")
+                    dispatchGroup.enter()
+                    APIManager.requestRandomImage(breed: breed) { (image, error) in
+                        if let breedModel = image, error == nil {
+                            let breedImage = BreedImage(image: breedModel.message, breed: breed)
+                            self.allBreedImage.append(breedImage)
+                        }
+                        dispatchGroup.leave()
+                    }
                 }
             }
-//        }
-        
+        }
+        sleep(2)
         dispatchGroup.notify(queue: .main) {
             print("end")
             completionHandler(self.allBreedImage, nil)
-            
         }
         dispatchGroup.wait()
     }
     
-    func fetchRandomImageForBreed(breed: String) -> [BreedImage] {
-        APIManager.requestRandomImage(breed: breed) { (image, err) in
-            print("image")
-            let img = BreedImage(image: image!.message, breed: breed)
-            self.allBreedImage.append(img)
-            return self.allBreedImage
+    func fetchRandomImageForBreed(breed: String, completionHandler: @escaping (BreedImage, Error?) -> Void) {
+        APIManager.requestRandomImage(breed: breed) { (image, error) in
+            if let breedModel = image, error == nil {
+                let breedImage = BreedImage(image: breedModel.message, breed: breed)
+                completionHandler(breedImage, nil)
+            }
         }
-        return []
     }
-    
-//    func fetchBreeds() {
-//        apiManager.getDataFromServer(endPoint.stringValue) { (model: DogImage ) in
-//            if model.status == "success" {
-//
-//            }
-//        }
-//    }
 }
 
